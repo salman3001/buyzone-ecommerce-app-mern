ddc3ab955b717e82110dcd58e0d3c2aaa75d12cd
salman <therodfighter>
2023-01-23 05:56:18 +0400

    migrated to vite js

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -42,9 +42,9 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 		transition: theme.transitions.create('width'),
 		width: '100%',
 		[theme.breakpoints.up('sm')]: {
-			width: '12ch',
+			width: '100%',
 			'&:focus': {
-				width: '20ch',
+				width: '100%',
 			},
 		},
 	},
